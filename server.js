4131270b88237d5eb9c6eb81fa770ebdcdd51878
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-10-29 21:11:41 -0400

    instantiated the dotenv stuff

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 // initialize the installation of expressJS & package.json
+const express = require('express'); 
+const app = express(); 
+
+//initialize the env stuff here as such 
+require('dotenv').config(); 
+
+//set up basic middleware for incoming data 
+app.use(express.json());  
+
