8ec0ac63a55a8484af8661ce19fe195e379f2f9e
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-10-30 22:49:28 -0400

    working on this

--- routes/parseroute.js
+++ routes/parseroute.js
@@ -1,13 +1,28 @@
 // this route is for the actual parsing of files from what is uploaded. 
 const express = require('express'); 
 const parserRoute = express.Router();  
-// in this route need to set up multer  
+// in this route need to set up multer   
+const multer = require('multer');  
+const path = require('path');  
+const fs = require('fs');  
+
+// set destination of where files will be uploaded to. 
+const upload = multer({ 
+    dest: 'uploads/'
+}); 
 
 // set up the route as such for parsing. 
-parserRoute.post('/parsefile', (req, res) => { 
-    res.send({ 
-        RouteForParsing: true
-    }); 
+parserRoute.post('/parsefile', upload.single('UserFile'), (req, res) => { 
+    // set up the file object as such where in this case it will be userResume 
+    const UserFile = req.UserFile;  
+
+    // handle error where if there is no file return a json error as such 
+    if (!UserFile) { 
+        return res.status(400).json({ 
+            error: 'No File was Uploaded'
+        }); 
+    } 
+
 }); 
 
 module.exports = parserRoute; 
